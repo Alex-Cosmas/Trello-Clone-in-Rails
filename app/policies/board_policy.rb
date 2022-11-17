3be804a9e1aa877b1d4a3eeccc7bf6fcbeae08bb
Alex Cosmas <alex.otieno@simpleformations.ke>
2022-11-17 18:15:51 +0300

    Updating a board

--- app/policies/board_policy.rb
+++ app/policies/board_policy.rb
@@ -2,13 +2,23 @@ class BoardPolicy < ApplicationPolicy
 
   class Scope < Scope
 
+
+
     # NOTE: Be explicit about which records you allow access to!
+
+
 
     # def resolve
 
+
+
     #   scope.all
 
+
+
     # end
+
+
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "dashboard#index"
 
-  resources :boards, only: [:new, :edit, :create]
+  resources :boards, only: [:new, :edit, :create, :update]
 end
